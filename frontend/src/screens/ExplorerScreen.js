67ed152c57cae8f5e7d1fe3849b1df946ff0eff2
Dylan-Ay <dylan.ayache.68@gmail.com>
2026-01-25 18:20:11 +0100

    Ajouts de commentaires dans ExplorerScreen et VisitScreen qui permettent d'établir les prochaines fonctionnalités par screen

--- frontend/src/screens/ExplorerScreen.js
+++ frontend/src/screens/ExplorerScreen.js
@@ -1,9 +1,11 @@
 import { ScreenWrapper, HeaderTitle } from "../components/ui"
 
 export const ExplorerScreen = ({ navigation }) => {
-  return (
-    <ScreenWrapper>
-      <HeaderTitle>Explorer</HeaderTitle>
-    </ScreenWrapper>
-  )
+   return (
+      <ScreenWrapper>
+         <HeaderTitle>Explorer</HeaderTitle>
+         {/* Liste de villes de la db avec barre de recherche, carousel vertical, clique sur la ville -> affichage des bâtiments -> clique sur bâtiment -> détail du bâtiment. 
+            Options : fonctions de tri et de filtre*/}
+      </ScreenWrapper>
+   )
 }
--- frontend/src/screens/VisitScreen.js
+++ frontend/src/screens/VisitScreen.js
@@ -4,6 +4,8 @@ export const VisitScreen = ({ navigation }) => {
   return (
     <ScreenWrapper>
       <HeaderTitle>A Visiter</HeaderTitle>
+      {/* Récupération des bâtiments ajoutés en favori -> détail du bâtiment. 
+      Options(Filtre, recherche) */}
     </ScreenWrapper>
   )
 }
